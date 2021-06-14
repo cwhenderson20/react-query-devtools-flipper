35c3fd36b49b6a81440701c341de4c8ae801b41c
Chris Henderson <chris@gridwise.io>
2021-06-14 11:40:27 -0400

    feat: query details panel with query status label

--- packages/flipper-plugin-react-query-devtools/src/components/Sidebar.tsx
+++ packages/flipper-plugin-react-query-devtools/src/components/Sidebar.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import type { Query, QueryKey } from "react-query";
-import { Panel, DetailSidebar, ManagedDataInspector, Toolbar } from "flipper";
-import { Button } from "antd";
+import { Panel, DetailSidebar, ManagedDataInspector, Layout } from "flipper";
+import { Button, Tag, Typography } from "antd";
+import { getQueryStatusColor, getQueryStatusLabel } from "../utils";
 
 type Props = {
   query: Query;
@@ -20,14 +21,51 @@ export default function Sidebar({
 }: Props) {
   return (
     <DetailSidebar minWidth={300} width={350}>
-      <Panel floating={false} heading="Query Details">
-        <ManagedDataInspector
-          data={{ "Query Key": query.queryKey }}
-          expandRoot={true}
-        />
+      <Panel
+        floating={false}
+        heading="Query Details"
+        accessory={
+          <div
+            style={{
+              display: "flex",
+              flex: 1,
+              alignItems: "center",
+              justifyContent: "flex-end",
+            }}
+          >
+            <Tag color={getQueryStatusColor(query)} style={{ marginRight: 0 }}>
+              {getQueryStatusLabel(query)}
+            </Tag>
+          </div>
+        }
+      >
+        <Layout.Container gap="tiny">
+          <Layout.Horizontal gap style={{ justifyContent: "space-between" }}>
+            <Typography.Text code>
+              {JSON.stringify(query.queryKey, null, 2)}
+            </Typography.Text>
+          </Layout.Horizontal>
+          <Layout.Horizontal style={{ justifyContent: "space-between" }}>
+            <Typography.Text>Observers:</Typography.Text>
+            <Typography.Text code>
+              {
+                // @ts-ignore: the `observers` property is available; can't use
+                // getObserversCount because the query object has been
+                // stringified and parsed
+                query.observers?.length ?? 0
+              }
+            </Typography.Text>
+          </Layout.Horizontal>
+          <Layout.Horizontal style={{ justifyContent: "space-between" }}>
+            <Typography.Text>Last Updated:</Typography.Text>
+            <Typography.Text code>
+              {new Date(query.state.dataUpdatedAt).toLocaleTimeString()}
+            </Typography.Text>
+          </Layout.Horizontal>
+        </Layout.Container>
       </Panel>
       <Panel floating={false} heading="Actions">
-        <Toolbar>
+        <Layout.Horizontal gap>
           <Button
             size="small"
             type="primary"
@@ -63,9 +101,13 @@ export default function Sidebar({
           >
             Remove
           </Button>
-        </Toolbar>
+        </Layout.Horizontal>
       </Panel>
-      <Panel floating={false} heading="Data Explorer">
+      <Panel
+        floating={false}
+        heading="Data Explorer"
+        collapsed={!query.state.data}
+      >
         <ManagedDataInspector
           data={query.state?.data}
           expandRoot={true}
--- packages/flipper-plugin-react-query-devtools/src/utils.ts
+++ packages/flipper-plugin-react-query-devtools/src/utils.ts
@@ -1,1 +1,40 @@
-export type { Query } from "react-query";
+import type { Query } from "react-query";
+
+export function getQueryStatusLabel(query: Query) {
+  return query.state.isFetching
+    ? "fetching"
+    : !getObserversCount(query)
+    ? "inactive"
+    : getIsStale(query)
+    ? "stale"
+    : "fresh";
+}
+
+const theme = {
+  gray: "#3f4e60",
+  success: "#00ab52",
+  active: "#006bff",
+  warning: "#ffb200",
+};
+
+export function getQueryStatusColor(query: Query) {
+  return query.state.isFetching
+    ? theme.active
+    : getIsStale(query)
+    ? theme.warning
+    : !getObserversCount(query)
+    ? theme.gray
+    : theme.success;
+}
+
+function getIsStale(query: Query) {
+  return (
+    query.state.isInvalidated ||
+    !query.state.dataUpdatedAt ||
+    query.observers?.some((observer) => observer.currentResult.isStale)
+  );
+}
+
+function getObserversCount(query: Query) {
+  return query.observers?.length ?? 0;
+}
